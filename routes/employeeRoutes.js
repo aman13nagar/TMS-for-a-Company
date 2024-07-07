ec80292bced8c457b0b2ce3726a212209254728b
Aman Nagar <122695931+aman13nagar@users.noreply.github.com>
2024-07-07 11:32:54 +0530

    Update employeeRoutes.js

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const employeeController = require('../controllers/employeeControlloer');
+const Task=require('../models/TaskModal');
 
 router.get('/tasks', employeeController.getEmployeeTasks);
+router.get('/tasks-tracking', async (req, res) => {
+    try {
+      const tasks = await Task.find();
+      res.status(200).send(tasks);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+});
 router.post('/complete-task/:taskId', employeeController.completeTask);
 module.exports = router;
